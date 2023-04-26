2043798cc24f0bc447c1c6bedd6066ca29700cbb
Mxk01 <RLuntraru22@gmail.com>
2023-04-26 23:41:33 +0300

    =

--- src/app/dashboard/Post.tsx
+++ src/app/dashboard/Post.tsx
@@ -15,8 +15,8 @@ function Post({id,title,rating}:PostProps) {
     let someFunc = (a:string)=>{
         setThatTitle(a)
         }
-    let doSomething = (e:React.InputHTMLAttributes<HTMLInputElement>) => {
-      console.log(e.formTarget?.valueOf)
+    let doSomething = (e:React.ChangeEvent<HTMLInputElement>) => {
+      console.log(e.target?.value)
     }
   return (
     <div>
